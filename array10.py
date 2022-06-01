11764b2379fae445e4e9083b79bee4a828eb200a
Umidjon <mrumidjon2020@gmail.com>
2022-06-01 11:07:21 +0500

    Create a 3x8 array filled with zeros values.

--- array10.py
+++ array10.py
@@ -6,7 +6,8 @@ def main():
     Create a 3x8 array filled with zeros values.
 
     """
-    return np.full((3, 8), fill_value=0)
+    # return np.full((3, 8), fill_value=0)
+    return np.zeros((3, 8))
 
 
-print(main())
+# print(main())
