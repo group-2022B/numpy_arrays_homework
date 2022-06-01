ac51949bdbe07be67d92f6d15800e8b1e3310df7
Umidjon <mrumidjon2020@gmail.com>
2022-06-01 10:57:12 +0500

    Create a 5x5 zero matrix with elements on the main diagonal equal to 1, 2, 3, 4, 5.

--- array07.py
+++ array07.py
@@ -1,8 +1,15 @@
 import numpy as np
 
+
 def main():
     """
     Create a 5x5 zero matrix with elements on the main diagonal equal to 1, 2, 3, 4, 5.
 
     """
-    return
+    return np.array([
+        [1, 0, 0, 0, 0],
+        [0, 2, 0, 0, 0],
+        [0, 0, 3, 0, 0],
+        [0, 0, 0, 4, 0],
+        [0, 0, 0, 0, 5]
+    ])
