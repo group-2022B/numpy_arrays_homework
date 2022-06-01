70a013551d1ac8d23909f81f556c1e90c22b6493
Umidjon <mrumidjon2020@gmail.com>
2022-06-01 10:38:03 +0500

    Create a 3x4 matrix filled with values from 10 to 21.

--- array04.py
+++ array04.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+
 def main():
     """
     Create a 3x4 matrix filled with values from 10 to 21.
 
     """
-    return
+    return np.array([
+        [10, 11, 12, 13],
+        [14, 15, 16, 17],
+        [18, 19, 20, 21]
+    ])
