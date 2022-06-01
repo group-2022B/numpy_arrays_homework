d3be4198f1d9a2d153a50f4fe846dccd762e382d
Umidjon <mrumidjon2020@gmail.com>
2022-06-01 10:34:23 +0500

    Create a 3x3 matrix with values from 2 to 10.

--- array02.py
+++ array02.py
@@ -5,7 +5,11 @@ def main():
     """
     Create a 3x3 matrix with values from 2 to 10.
     """
-    return np.arange(2, 11).reshape(3, 3)
-
+    # return np.arange(2, 11).reshape(3, 3)
+    return np.arange([
+        [2, 3, 4],
+        [5, 6, 7],
+        [8, 9, 10]
+    ])
 
 # print(main())
