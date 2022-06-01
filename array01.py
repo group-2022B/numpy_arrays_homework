89f5bc76248f5d82e91e8a8c77f715606d4df857
Umidjon <mrumidjon2020@gmail.com>
2022-06-01 10:26:35 +0500

    Convert the list of numeric values to a one-dimensional NumPy array.

--- array01.py
+++ array01.py
@@ -5,5 +5,5 @@ def main(array):
     """
     Convert the list of numeric values to a one-dimensional NumPy array.
     """
-    a = np.array(array, dtype=np.float8)
+    a = np.array(array)
     return a
